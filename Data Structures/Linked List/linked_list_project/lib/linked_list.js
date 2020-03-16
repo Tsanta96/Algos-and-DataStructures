7dc2c21b20930519c6ba8f0916820111ab225d6a
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-16 12:54:36 -0400

    Singly Linked List Implementation

--- Data Structures/Linked List/linked_list_project/lib/linked_list.js
+++ Data Structures/Linked List/linked_list_project/lib/linked_list.js
@@ -35,33 +35,123 @@ class LinkedList {
     }
 
     addToTail(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+        this.length++;
+        return this;
     }
 
     removeTail() {
+        if (!this.head) return undefined;
+
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
     }
 
     addToHead(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+        let tempNode = this.head;
+        this.head = newNode;
+        this.head.next = tempNode;
+        }
+        this.length++;
+        return this;
     }
 
     removeHead() {
+        if (!this.head) return undefined;
+        let head = this.head;
+        if (this.head.next === null) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+        this.length--;
+        return head;
     }
 
     contains(target) {
+        if (this.head === target) return true;
+
+        let current = this.head;
+        while (current) {
+            if (current.value === target) return true;
+            current = current.next;
+        }
+        return false;
     }
 
     get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (current) {
+            if (counter === index) return current;
+            if (!current.next) return null;
+            current = current.next;
+            counter++;
+        }
     }
 
     set(index, val) {
+        const foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.value = val;
+            return true;
+        }
+        return false;
     }
 
     insert(index, val) {
+        if (index < 0 || index >= this.length) return false;
+        if (index === this.length) return !!this.addToTail(val);
+        if (index === 0) return !!this.addToHead(val);
+        
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        const temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
     }
 
     remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === this.length) return this.removeTail;
+        if (index === 0) return this.removeHead;
+
+        const remNode = this.get(index);
+        const prevNode = this.get(index-1);
+        prevNode.next = prevNode.next.next;
+        this.length--;
+        return remNode;
     }
 
     size() {
+        return this.length;
     }
 }
 
