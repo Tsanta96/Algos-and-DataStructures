5dc4ad6ae0cdb39b0f029b3e5e653dc85f5e840d
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-18 14:07:54 -0400

    Add MinMaxStack

--- Data Structures/Stacks & Queues/stack_queue_interview_problems/lib/2_min_max_stack.js
+++ Data Structures/Stacks & Queues/stack_queue_interview_problems/lib/2_min_max_stack.js
@@ -72,44 +72,73 @@ class Node {
 }
 
 // Refactor the regular Stack below into a MinMaxStack!
-class Stack {
+class MinMaxStack {
     constructor() {
         this.top = null;
         this.bottom = null;
         this.length = 0;
+        this.maxValueStorage = [];
+        this.minValueStorage = [];
     }
 
-    push(val) {
-        const newNode = new Node(val);
-        if (!this.top) {
-            this.top = newNode;
-            this.bottom = newNode;
-        } else {
-            const temp = this.top;
-            this.top = newNode;
-            this.top.next = temp;
+        push(val) {
+            const newNode = new Node(val);
+
+            if (!this.minValueStorage.length || newNode.value <= this.minValueStorage[this.minValueStorage.length - 1].value) {
+                this.minValueStorage.push(newNode);
+            }
+
+            if (!this.maxValueStorage.length || newNode.value >= this.maxValueStorage[this.maxValueStorage.length - 1].value) {
+                this.maxValueStorage.push(newNode);
+            }
+
+            if (!this.top) {
+                this.top = newNode;
+                this.bottom = newNode;
+            } else {
+                const temp = this.top;
+                this.top = newNode;
+                this.top.next = temp;
+            }
+            return ++this.length;
         }
-        return ++this.length;
-    }
 
     pop() {
         if (!this.top) {
             return null;
         }
+
+        if (this.top.value === this.maxValueStorage[this.maxValueStorage.length - 1].value) {
+            this.maxValueStorage.pop();
+        }
+
+        if (this.top.value === this.minValueStorage[this.minValueStorage.length - 1].value) {
+            this.minValueStorage.pop();
+        }
+
+
         const temp = this.top;
         if (this.top === this.bottom) {
             this.bottom = null;
         }
         this.top = this.top.next;
         this.length--;
-        return temp.value;
+        return temp;
     }
 
     size() {
         return this.length;
     }
+
+    min() {
+        return this.minValueStorage[this.minValueStorage.length - 1] || null;
+    }
+
+    max() {
+        return this.maxValueStorage[this.maxValueStorage.length - 1] || null;
+    }
 }
 
 // Forgetting something down here? 
 exports.Node = Node;
-exports.Stack = Stack;
+exports.MinMaxStack = MinMaxStack;
