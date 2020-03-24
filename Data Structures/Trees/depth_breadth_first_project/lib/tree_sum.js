5a8c8e3d9416f4ca7aceed4a229afd042241862c
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-24 15:50:16 -0400

    Add tree_sum.js

--- Data Structures/Trees/depth_breadth_first_project/lib/tree_sum.js
+++ Data Structures/Trees/depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,15 @@
 function treeSum(root) {
+    if (!root) return 0;
 
+    let treeSum = 0;
+    let queue = [root];
+    while (queue.length) {
+        let node = queue.pop();
+        treeSum += node.val;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return treeSum;
 }
 
 
