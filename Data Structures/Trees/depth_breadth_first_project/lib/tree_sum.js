2ca1474ed3d460824c9ab6075f2c6c64e6471ca8
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-24 16:02:39 -0400

    Solved tree_sum dfs and bfs

--- Data Structures/Trees/depth_breadth_first_project/lib/tree_sum.js
+++ Data Structures/Trees/depth_breadth_first_project/lib/tree_sum.js
@@ -1,15 +1,44 @@
+//bfs approach
+// function treeSum(root) {
+//     if (!root) return 0;
+
+//     let treeSum = 0;
+//     let queue = [root];
+//     while (queue.length) {
+//         let node = queue.shift();
+//         treeSum += node.val;
+//         if (node.left) queue.push(node.left);
+//         if (node.right) queue.push(node.right);
+//     }
+//     return treeSum;
+// }
+
+//dfs iterative approach
+// function treeSum(root) {
+//     if (!root) return 0;
+
+//     let treeSum = 0;
+//     let stack = [ root ];
+//     while (stack.length) {
+//         let node = stack.pop();
+//         treeSum += node.val;
+//         if (node.right) stack.push(node.right);
+//         if (node.left) stack.push(node.left);
+//     }   
+//     return treeSum;
+// }
+
+
+//dfs recursive approach
 function treeSum(root) {
     if (!root) return 0;
 
-    let treeSum = 0;
-    let queue = [root];
-    while (queue.length) {
-        let node = queue.pop();
-        treeSum += node.val;
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
-    }
-    return treeSum;
+    let sum = 0;
+    sum += root.val;
+    sum += treeSum(root.left); 
+    sum += treeSum(root.right); 
+
+    return sum; 
 }
 
 
