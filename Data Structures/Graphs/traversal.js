4f67dde900d479b57a2c7e66fc8d1db57a31d7c0
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-26 14:39:32 -0400

    Add Djikstras algo

--- Data Structures/Graphs/traversal.js
+++ Data Structures/Graphs/traversal.js
@@ -1,4 +1,4 @@
-//broken
+//Broken (gets caught in a loop)
 function depthFirstRecur(node) {
     console.log(node.val);
 
@@ -7,6 +7,7 @@ function depthFirstRecur(node) {
     })
 }
 
+//Better recursive version 
 function depthFirstRecur(node, visited=new Set()) {
     // if this node has already been visited, then return early
     if (visited.has(node.val)) return;
@@ -19,5 +20,53 @@ function depthFirstRecur(node, visited=new Set()) {
     // then explore each of its neighbors
     node.neighbors.forEach(neighbor => {
         depthFirstRecur(neighbor, visited);
+    })
+}
+
+//Better iterative version
+function depthFirstIter(node) {
+    let visited = new Set();
+    let stack = [ node ];
+
+    while (stack.length) {
+        let node = stack.pop();
+        if (visited.has(node.val)) continue;
+
+        console.log(node.val);
+        visited.add(node.val);
+
+        stack.push(...node.neighbors);
+    }
+}
+
+//Using Adjacency List representation
+function depthFirstRecur(node, graph, visited=new Set()) {
+    if (visited.has(node)) return;
+
+    console.log(node);
+    visited.add(node);
+
+    graph[node].forEach(neighbor => {
+        depthFirstRecur(neighbor, graph, visited);
+    });
+}
+
+//When choosing a node that isn't connected to all other nodes
+function depthFirst(graph) {
+    let visited = new Set();
+
+    for (let node in graph) {
+        _depthFirstRecur(node, graph, visited);
+    }
+}
+
+function _depthFirstRecur(node, graph, visited) {
+    if (visited.has(node)) return;
+
+    console.log(node);
+    visited.add(node);
+
+    graph[node].forEach(neighbor => {
+        _depthFirstRecur(neighbor, graph, visited);
     })
 }
