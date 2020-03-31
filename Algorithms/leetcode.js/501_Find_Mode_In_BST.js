d44d89d63725358ac2c57c118c419dd14b775c4a
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-31 15:42:04 -0400

    Add leetcode 501

--- Algorithms/leetcode.js/501_Find_Mode_In_BST.js
+++ Algorithms/leetcode.js/501_Find_Mode_In_BST.js
@@ -27,7 +27,43 @@ var findMode = function (root) {
     //Traverse the tree, add the value of each node to an array
     //create an object that stores the values of an array and the frequency that they appear.
     //Sort that object by values (frequency) return the most frequent
+
+    //create a Map object
+    const counts = new Map();
+    //set the max number of the same value.
+    const max = walk(root, counts);
+    //initialize an empty array
+    const result = [];
+
+    //iterate through counts with the deconstructed value and count
+    for (let [value, count] of counts) {
+        //if the count is equal to the max, push that value into the result
+        //The pushed result means it has the same number of reoccuring elements as the max
+        if (count === max) {
+            result.push(value);
+        }
+    }
+
+    return result;
 };
+
+const walk = (root, counts) => {
+    //This helper function populates the Map() and returns the max number of 
+    //reocurring elements
+    if (!root) return 0;
+    
+    //increment count for each reoccurence of a value
+    const count = counts.has(root.val) ? counts.get(root.val) + 1 : 1
+    //set the the key to the root.val and the value to the count
+    counts.set(root.val, count);
+
+    //recursively call walk and eventually return the max number of reoccuring elements
+    return Math.max(
+        count,
+        walk(root.left, counts),
+        walk(root.right, counts)
+    );
+}
 
 let input = [1, null, 2, 2]
 console.log(findMode(input));
