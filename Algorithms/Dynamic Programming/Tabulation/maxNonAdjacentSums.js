7bb3b2dc6b3eff1e496cadf087124b0893c44514
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-09 13:21:15 -0400

    Add minChange

--- Algorithms/Dynamic Programming/Tabulation/maxNonAdjacentSums.js
+++ Algorithms/Dynamic Programming/Tabulation/maxNonAdjacentSums.js
@@ -8,6 +8,35 @@
 //
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
-function maxNonAdjacentSum(nums) {
 
+//tabulation
+function maxNonAdjacentSum(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    let table = [];
+    table[0] = nums[0];
+    table[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < nums.length; i++) {
+        table[i] = Math.max(table[i-2] + nums[i], table[i-1]);
+    }
+    return table[table.length-1];
 }
+
+//memoization
+function maxNonAdjacentSum(nums, memo={}) {
+    if (nums.length in memo) return memo[nums.length];
+    if (nums.length === 0) return 0;
+
+    let firstEle = nums[0];
+    memo[nums.length] = Math.max(
+        firstEle + maxNonAdjacentSum((nums.slice(2)), memo),
+        maxNonAdjacentSum((nums.slice(1)), memo)
+    );
+
+    return memo[nums.length];
+}   
+
+console.log(maxNonAdjacentSum([4, 2, 1, 6]))
--- Algorithms/Dynamic Programming/Tabulation/minChange.js
+++ Algorithms/Dynamic Programming/Tabulation/minChange.js
@@ -11,5 +11,18 @@
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount) {
+    let table = new Array(amount + 1).fill(Infinity);
+    let table[0] = 0;
 
+    coins.forEach((coin) => {
+        for(let amt = 0; amt < table.length; amt++) {
+            for (let qty = 0; qty * val <= amt; qty++) {
+                remainder = amt - qty * val
+                let attempt = table[remainder] + qty;
+                if (attempt < table[amt]) table[amt] = attempt;
+            }
+        }
+    })
+
+    return table[table.length - 1];
 }
