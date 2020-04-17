4ff41c196b48cc77b9eea922cc810bddae7e14bd
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-17 12:38:06 -0400

    Add Valid_Palindrome

--- Algorithms/leetcode/Top Interview Questions/Strings/Valid_Palindrome.js
+++ Algorithms/leetcode/Top Interview Questions/Strings/Valid_Palindrome.js
@@ -11,3 +11,24 @@
 // Input: "race a car"
 // Output: false
 
+var isPalindrome = function (s) {
+    if (s.length === 0) return true;
+
+    let alphNums = "abcdefghijklmnopqrstuvwxyz0123456789"
+    let palArr = [];
+
+    let lowCaseStr = s.toLowerCase();
+
+    for (let i = 0; i < s.length; i++) {
+        if (alphNums.includes(lowCaseStr[i])) {
+            palArr.push(lowCaseStr[i]);
+        }
+    }
+    return palArr.join('') === palArr.reverse().join('');
+};
+//lowercase
+//remove non alphanumeric characters
+//remove spaces
+
+
+console.log(isPalindrome("race a car"));
