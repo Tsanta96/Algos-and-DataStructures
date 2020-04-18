70acf1a8234ba6d9122540b220b3a6f6e7686eab
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-18 10:35:56 -0400

    Add Implement_strStr()

--- Algorithms/leetcode/Top Interview Questions/Strings/String_To_Integer.js
+++ Algorithms/leetcode/Top Interview Questions/Strings/String_To_Integer.js
@@ -84,6 +84,15 @@ var myAtoi = function (str) {
     return theNum;
 };
 
+//MUCH EASIER AND MORE STRAIGHT FORWARD WAY:
+const INT_MIN = Math.pow(-2, 31),
+      INT_MAX = Math.pow(2, 31) - 1;
+
+var myAtoi = function (str) {
+    const atoi = parseInt(str.trim().split(' ')[0]);
+    return Number.isInteger(atoi) ? Math.max(INT_MIN, Math.min(INT_MAX, atoi)) : 0;
+};
+
 console.log(myAtoi("+"));
 
 //Input
