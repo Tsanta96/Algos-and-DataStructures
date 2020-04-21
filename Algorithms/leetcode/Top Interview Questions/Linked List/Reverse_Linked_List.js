d315ec070a64ff0d9b4f00c11a7c5e303411055d
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-21 11:51:39 -0400

    Add Reverse_Linked_List

--- Algorithms/leetcode/Top Interview Questions/Linked List/Reverse_Linked_List.js
+++ Algorithms/leetcode/Top Interview Questions/Linked List/Reverse_Linked_List.js
@@ -12,7 +12,7 @@
 var reverseList = function (head) {
     let prev = null;
     let curr = head;
-    while (curr != null) {
+    while (curr !== null) {
         let nextTemp = curr.next;
         curr.next = prev;
         prev = curr;
@@ -20,19 +20,36 @@ var reverseList = function (head) {
     }
     return prev;
 };
-
-//recursive
-var reverseList = function (head) {
-    if (head == null || head.next == null) return head;
-    let p = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return p;
-};
 //Approach: While you are traversing the list, change the current node's next pointer to point to its previous element. 
 //since a node does not have reference to its previous node (singly linked list), you must store its previous element beforehand.
 //You also need another pointer to store the next node before changing the reference. Do not forget to return the new head
 //reference at the end.
+
+//recursive
+function reverseList(head) {
+    if (head === null || head.next === null) return head;
+    let p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
+// Approach: The recursive version is slightly trickier and the key is to work 
+// backwards.Assume that the rest of the list had already been reversed, now how do 
+// I reverse the front part ? Let 's assume the list is: n1 → … → nk-1 → nk → nk+1 → … → nm → Ø
+
+// Assume from node nk + 1 to nm had been reversed and you are at node nk.
+
+// n1→…→ nk - 1→ nk→ nk + 1←…← nm
+
+// We want nk + 1’ s next node to point to nk.
+
+// So,
+
+// nk.next.next = nk;
+
+// Be very careful that n1 's next must point to Ø. If you forget about this, your 
+// linked list has a cycle in it. This bug could be caught if you test your code with a linked list of size 2.
+
 
 //naive approach:
 //iterate through linkedlist and shifting each node into a new linked list to be returned
