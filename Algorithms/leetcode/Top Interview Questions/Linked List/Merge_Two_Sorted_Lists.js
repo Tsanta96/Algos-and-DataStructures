c2244aebb06331556abc84f971a5867fb3511d01
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-22 13:26:57 -0400

    Add Merge_Two_Sorted_Lists

---
+++ Algorithms/leetcode/Top Interview Questions/Linked List/Merge_Two_Sorted_Lists.js
@@ -0,0 +1,25 @@
+
+// Merge two sorted linked lists and return it as a new list. The new list should 
+// be made by splicing together the nodes of the first two lists.
+
+// Example:
+
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+}
+//Input = 2 Linked lists
+//Output = 1 merged linked list
+//Tools 
+//Strategy
+
+//Execute
